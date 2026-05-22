a687a60814a37420c85ed988044488b4870ade28
wonhou <wiehwjr3@gmail.com>
2026-05-22 17:37:21 +0900

    Revert "Feature/iam"

--- infra/backend.tf
+++
@@ -1,9 +0,0 @@
-# terraform {
-#   backend "s3" {
-#     bucket         = "team4-terraform-state"
-#     key            = "vpc/terraform.tfstate"
-#     region         = "ap-northeast-2"
-#     dynamodb_table = "team4-terraform-lock"
-#     encrypt        = true
-#   }
-# }
--- infra/modules/iam/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "s3_bucket_name" {
-  description = "생성된 IRSA 테스트용 S3 버킷 이름"
-  value       = aws_s3_bucket.irsa_test.bucket
-}
-
-output "s3_reader_role_arn" {
-  description = "쿠버네티스 ServiceAccount에 어노테이션으로 등록할 IAM Role ARN"
-  value       = aws_iam_role.s3_reader.arn
-}
