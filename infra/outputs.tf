c50c8dd08763bc46247da1613d4237f2d32c3727
wonhou <wiehwjr3@gmail.com>
2026-05-22 16:46:18 +0900

    feat: EKS IRSA 테스트 모듈 추가

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -17,3 +17,13 @@ output "db_subnet_ids" {
   description = "DB Subnet ID 목록"
   value       = module.vpc.db_subnet_ids
 }
+
+output "iam_test_s3_bucket" {
+  description = "내가 만든 모듈의 S3 버킷 이름"
+  value       = module.iam.s3_bucket_name
+}
+
+output "iam_test_irsa_role" {
+  description = "내가 만든 모듈의 IAM Role ARN"
+  value       = module.iam.s3_reader_role_arn
+}
