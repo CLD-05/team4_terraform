12c18ffbe9c86cb87bf4058be8295dc85281c0e7
wonhou <wiehwjr3@gmail.com>
2026-05-26 10:25:19 +0900

    feat: update iam terraform configuration

--- infra/modules/iam/variables.tf
+++ infra/modules/iam/variables.tf
@@ -1,14 +1,15 @@
 variable "project_name" {
-  type        = string
-  description = "프로젝트 이름 prefix (예: team4)"
+  type = string
 }
 
 variable "oidc_provider_arn" {
-  type        = string
-  description = "EKS 모듈에서 출력(Output)된 IAM OpenID Connect Provider ARN"
+  type = string
 }
 
 variable "oidc_provider_url" {
-  type        = string
-  description = "EKS 모듈에서 출력(Output)된 EKS 클러스터의 OIDC Issuer URL"
+  type = string
+}
+
+variable "github_repo" {
+  type = string
 }
