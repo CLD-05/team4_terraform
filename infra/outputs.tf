a1a81ffb44128fdc96d391db05fd6e5fc11014d5
potent93 <potent93@gmail.com>
2026-05-26 10:22:08 +0900

    fix: outputs.tf merge conflict 해결

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -31,13 +31,4 @@ output "eks_node_sg_id" {
 output "rds_sg_id" {
   description = "RDS Security Group ID"
   value       = module.vpc.rds_sg_id
-  
-output "iam_test_s3_bucket" {
-  description = "내가 만든 모듈의 S3 버킷 이름"
-  value       = module.iam.s3_bucket_name
-}
-
-output "iam_test_irsa_role" {
-  description = "내가 만든 모듈의 IAM Role ARN"
-  value       = module.iam.s3_reader_role_arn
 }
